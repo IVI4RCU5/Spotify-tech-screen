b948308c2f180e7f8af528454dab086d98645415
Marcus Hausler <marcus.hausler@gmail.com>
2018-06-18 12:11:01 -0400

    Optimize and account for additional edge cases

--- Q1-sortByStrings.js
+++ Q1-sortByStrings.js
@@ -1,10 +1,16 @@
 function sortByStrings(s,t) {
   let result = ''
-  for (var i = 0; i < t.length; i++) {
-    for (var j = 0; j < s.length; j++) {
-      if (s[j] === t[i]) {
-        result += t[i]
-      }
+  let storage = {}
+  for (var i = 0; i < s.length; i++) {
+    if (storage[s[i]]) {
+      storage[s[i]]++
+    } else {
+      storage[s[i]] = 1
+    }
+  }
+  for (var j = 0; j < t.length; j++) {
+    if (storage[t[j]]) {
+      result += t[j].repeat(storage[t[j]])
     }
   }
   return result
--- Q2-decodeString.js
+++ Q2-decodeString.js
@@ -1,7 +1,32 @@
-function decodeString(s) {
+var decodeString = function(s) {
   if (s.indexOf('[') < 0) return s
-  let num = s.indexOf('[') - 1
-  let start = s.indexOf('[') + 1
-  let end = s.length - s.split('').reverse().indexOf(']') - 1
-  return s.substring(0, num) + decodeString(s.substring(start, end)).repeat(s[num])
-}
+  let brackets = 0
+  let substrings = []
+  let pointer = 0
+  for (var i = 0; i < s.length; i++) {
+      if (s[i] === '[') {
+          brackets++
+      } else if (s[i] === ']') {
+          brackets--
+          if (brackets === 0) {
+              substrings.push(s.slice(pointer, i+1))
+              pointer = i+1
+          }
+      }
+  }
+  let tail = s.slice(pointer)
+  return substrings.map((substr) => {
+      let openBracket = substr.indexOf('[')
+      let closeBracket = substr.length - substr.split('').reverse().indexOf(']') - 1
+      let prefix = ''
+      let num = ''
+      substr.slice(0, openBracket).split('').forEach((el) => {
+          if (parseInt(el) >= 0) {
+              num += el
+          } else {
+              prefix += el
+          }
+      })
+      return prefix + decodeString(substr.substring(openBracket+1, closeBracket)).repeat(parseInt(num))
+  }).join('') + tail
+};
